5b7f63b48bdd6f2e2b94a21ce56a084119ffc122
Xenation <xenations.111@gmail.com>
2019-02-10 02:17:00 +0100

    Fixed chunk mesh vertex count

--- CR_VoxelWorld/Chunk.cpp
+++ CR_VoxelWorld/Chunk.cpp
@@ -41,11 +41,23 @@ void fillCube(Vec3f* verts, int vIndex, unsigned int* indices, int iIndex, Vec3f
 }
 
 void Chunk::generateMesh() {
-	int vCount = CHUNK_VOXEL_COUNT * 8;
-	int iCount = CHUNK_VOXEL_COUNT * 36;
+	// Counting pass
+	int vCount = 0;
+	int iCount = 0;
+	for (int y = 0; y < CHUNK_SIZE; y++) {
+		for (int z = 0; z < CHUNK_SIZE; z++) {
+			for (int x = 0; x < CHUNK_SIZE; x++) {
+				if (voxels[zorder(Vec3c(x, y, z))].type == Voxel::Air) continue;
+				vCount += 8;
+				iCount += 36;
+			}
+		}
+	}
+
 	mesh = new Mesh(vCount, iCount);
 	mesh->setAttributesDefinition(1, new int {4});
 	
+	// Meshing pass
 	Vec3f* vertices = new Vec3f[vCount];
 	unsigned int* indices = new unsigned int[iCount];
 	int vIndex = 0;
